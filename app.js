c2dcad1f6f3fbe457e682ddc5bcaeccb37e95f79
Ella Moss <ella.moss@utah.edu>
2022-10-08 13:07:33 -0600

    naraaaaa

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-function focusSection(prevSection,nextSection){
+function focusSection(prevSection,nextSection,direction){
+	var prevDir = 
 	$("#"+prevSection).hide("slide",{direction:"left"},1000);
 	$("#"+nextSection).show("slide",{direction:"right"},1000);
 }
+
+$(document).ready(function(){
+	$(".btn").click(function(e){
+		var text = $(e.target).text();
+		var prev = $(e.target).attr("data-prev");
+		var next = $(e.target).attr("data-next");
+		focusSection(prev,next,text);
+	});
+});
